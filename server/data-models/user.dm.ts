f0cb31d981a35cc432fe1477ebbfbd5e8165f022
Dmytro Petrenko <dmg.petrenko@gmail.com>
2021-02-04 09:48:11 +0200

    added user data-model

---
+++ server/data-models/user.dm.ts
@@ -0,0 +1,54 @@
+import { Document, Model, model, Schema } from 'mongoose';
+
+export interface IUserDocument extends Document {
+  name: string;
+  email: string;
+  createdOn: Date;
+  updatedOn: Date;
+  hashed_password: string;
+  salt: string;
+}
+
+interface IUserModel extends Model<IUserDocument> {}
+
+const userSchema: Schema = new Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  email: {
+    type: String,
+    trim: true,
+    required: true,
+    unique: true,
+    validate: (value: string) => {
+      if (!value.match(/.+\@.+\..+/)) {
+        throw new Error('Please fill a valid email address');
+      }
+    },
+  },
+  createdOn: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedOn: {
+    type: Date,
+  },
+  hashed_password: {
+    type: String,
+    required: true,
+  },
+  salt: {
+    type: String,
+  },
+});
+
+userSchema
+  .virtual('password')
+  .set((password: string) => {
+    this._password = password;
+  })
+  .get(() => this._password);
+
+export const UserModel = model<IUserDocument, IUserModel>('User', userSchema, 'users');
