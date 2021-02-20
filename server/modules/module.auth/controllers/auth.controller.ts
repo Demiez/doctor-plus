7c01aeb263259126de04c7c95819a2fff31d989f
Dmytro Petrenko <dmg.petrenko@gmail.com>
2021-02-20 14:26:20 +0200

    model.auth - added signOut

--- server/modules/module.auth/controllers/auth.controller.ts
+++ server/modules/module.auth/controllers/auth.controller.ts
@@ -1,6 +1,7 @@
 import { CookieOptions, Request, Response } from 'express';
 import * as moment from 'moment';
 import BaseController from '../../../core/abstract/base-controller';
+import { BaseResponseViewModel } from '../../../core/route/responses/base-response.model';
 import { ModuleAuth_AuthService } from '../services/auth.service';
 import { SignInRequestViewModel } from '../view-models';
 
@@ -15,6 +16,12 @@ class AuthController extends BaseController {
     res.cookie('t', responseData.token, options);
 
     return this.sendSuccessResponse(res, responseData);
+  }
+
+  public async signOut(req: Request, res: Response) {
+    res.clearCookie('t');
+
+    return this.sendSuccessResponse(res, new BaseResponseViewModel('signed out', undefined));
   }
 }
 
