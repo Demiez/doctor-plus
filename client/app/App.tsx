94c0dfdfa6f95cba2b37359c1ed698cb4c0356ec
Dmytro Petrenko <dmg.petrenko@gmail.com>
2021-02-26 10:11:35 +0200

    FE - arch update

--- client/app/App.tsx
+++ client/app/App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Main } from './modules/Main';
+import { Main } from './core/Main';
 
 ReactDOM.render(<Main />, document.getElementById('root'));
--- client/app/core/MainRouter.tsx
+++ client/app/core/MainRouter.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { HomePage } from './module.home';
+import { HomePage } from '../pages/';
 
 export const MainRouter: React.FC = () => {
   return (
